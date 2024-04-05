a98f0d2fc2fe67b22c7b331675da8002e9853e42
Paul Pham <148553+learner-long-life@users.noreply.github.com>
2024-04-05 18:55:16 +0000

    read a FASTA file into rust program

--- rusty-plants/src/main.rs
+++ rusty-plants/src/main.rs
@@ -1,15 +1,17 @@
 use std::io::{stdin,BufRead};
 use regex_lite::Regex;
+use std::env;
 
 fn main() {
+    let args: Vec<String> = env::args().collect();
+    let contents = std::fs::read_to_string(&args[1]).unwrap();
     let tokenizer = Regex::new(r"([.,\s]+)").unwrap();
 
     let mut tokens: Vec<&str>;
 
-    let input stdin().lock();
-    let lines = input.lines();
+    let lines = contents.lines();
 
     for line in lines {
-
+        println!("{:?}",line);
     }
 }
