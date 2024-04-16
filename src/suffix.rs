78ade9d9755d96626dc2154c4378faec98ade663
Pope_Rigby <poperigby@mailbox.org>
2024-04-16 12:45:16 -0700

    Fix

--- src/suffix.rs
+++ src/suffix.rs
@@ -3,24 +3,21 @@
 use std::collections::HashMap;
 
 pub struct SuffixArray<'a> {
-    map: HashMap<&'a str, Vec<u8>>,
+    map: HashMap<&'a str, Vec<u32>>,
 }
 
-impl SuffixArray<'_> {
-    pub fn new(genome: &str, k_mer_size: usize) -> Self {
+impl<'a> SuffixArray<'a> {
+    pub fn new(genome: &'a str, k_mer_size: usize) -> Self {
         let mut map: HashMap<&str, Vec<u32>> = HashMap::new();
 
         for index in 0..genome.len() - k_mer_size {
-            //dbg!(&genome[index..index + k_mer_size]);
             let k_mer: &str = &genome[index..index + k_mer_size];
-            if let Some(mut indicies) = map.get(k_mer) {
+            if let Some(indicies) = map.get_mut(k_mer) {
                 indicies.push(index as u32);
             } else {
-                map.insert(k_mer, vec!(index as u32));
+                map.insert(k_mer, vec![index as u32]);
             };
         }
-        Self {
-            map: HashMap::new(),
-        }
+        Self { map }
     }
 }
