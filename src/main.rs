e68d95ea6ab46e2873a07a3e3210587656b40032
Gavin Bowers <supercat648@gmail.com>
2024-04-16 12:36:25 -0700

    added unfinished hashmap read alignment

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,20 @@
 use rusty_plants::fasta::{
     align, parse_file, parse_genome, read_directory_to_string};
 use std::fs::read_to_string;
+use rusty_plants::suffix::SuffixArray;
 
 fn main() {
     let args: Vec<String> = std::env::args().collect();
-    if args.len() < 2 {panic!("pants shidded - usage is fragments_dir genome_path");}
+    // if args.len() < 2 {panic!("pants shidded - usage is fragments_dir genome_path");}
 
-    let files = read_directory_to_string(&args[1]).expect("failed to read fragment files");
-    let fragments = parse_file(&files).expect("failed to parse fragments");
-    let genome = parse_genome(read_to_string(&args[2]).expect("failed to read genome file"));
+    // let files = read_directory_to_string(&args[1]).expect("failed to read fragment files");
 
-    let indicies: Vec<usize> = align::naive(genome, fragments);
-    println!("{:?}", indicies);
+    // let fragments = parse_file(&files).expect("failed to parse fragments");
+    let genome = parse_genome(read_to_string(&args[1]).expect("failed to read genome file"));
+    SuffixArray::new(&genome, 10);
+
+    // let indicies: Vec<usize> = align::naive(genome, fragments);
+    // println!("{:?}", indicies);
 
     //println!("{:?}", fragments[0]);
     //histogram(&fragments, 100, 100);
--- src/suffix.rs
+++ src/suffix.rs
@@ -2,12 +2,23 @@
 
 use std::collections::HashMap;
 
-struct SuffixArray<'a> {
+pub struct SuffixArray<'a> {
     map: HashMap<&'a str, Vec<u8>>,
 }
 
 impl SuffixArray<'_> {
-    pub fn new(genome: &str, k_mer_size: i32) -> Self {
+    pub fn new(genome: &str, k_mer_size: usize) -> Self {
+        let mut map: HashMap<&str, Vec<u32>> = HashMap::new();
+
+        for index in 0..genome.len() - k_mer_size {
+            //dbg!(&genome[index..index + k_mer_size]);
+            let k_mer: &str = &genome[index..index + k_mer_size];
+            if let Some(mut indicies) = map.get(k_mer) {
+                indicies.push(index as u32);
+            } else {
+                map.insert(k_mer, vec!(index as u32));
+            };
+        }
         Self {
             map: HashMap::new(),
         }
