8b864734cd8dd07aac24a06e5f4b0ca0a7333a47
Pope_Rigby <poperigby@mailbox.org>
2024-04-17 17:35:16 -0700

    Tree

--- src/tree.rs
+++ src/tree.rs
@@ -1,5 +1,23 @@
+#![allow(dead_code)]
+
 struct Leaf<'a, T> {
-    value: T,
+    value: Option<T>,
     left: Option<&'a Leaf<'a, T>>,
     right: Option<&'a Leaf<'a, T>>,
+}
+
+struct Tree<'a, T> {
+    root: &'a Leaf<'a, T>,
+}
+
+impl<T> Tree<'_, T> {
+    pub fn new() -> Self {
+        Self {
+            root: &Leaf {
+                value: None,
+                left: None,
+                right: None,
+            },
+        }
+    }
 }
