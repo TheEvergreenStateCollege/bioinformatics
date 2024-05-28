0e205bf7dc4f0907601c9d633f38fd422706ebab
Pope_Rigby <poperigby@mailbox.org>
2024-05-28 11:43:16 -0700

    Fix algorithm

--- src/algorithms/edit_distance.rs
+++ src/algorithms/edit_distance.rs
@@ -3,7 +3,7 @@ use crate::data_structures::matrix::Matrix;
 /// Return the minimum amount of changes needed to transform `s1` into `s2`.
 pub fn edit_distance(s1: &str, s2: &str) -> usize {
     // An extra row and column are required for the base cases
-    let mut m = Matrix::<usize>::new(s2.len() + 1, s1.len() + 1);
+    let mut m = Matrix::<usize>::new(s1.len() + 1, s2.len() + 1);
 
     // The base cases represent how many operations it takes to convert
     // an empty string into each substring.
@@ -18,30 +18,114 @@ pub fn edit_distance(s1: &str, s2: &str) -> usize {
         m.set(x, 0, x);
     }
 
+    // We start from 1 to skip over the base cases
     for y in 1..m.height() {
         for x in 1..m.width() {
             let replace_cost = *m.get(x - 1, y - 1).unwrap();
             let delete_cost = *m.get(x - 1, y).unwrap();
             let insert_cost = *m.get(x, y - 1).unwrap();
 
-            let mut edit_distance = *[replace_cost, delete_cost, insert_cost]
+            let min_cost = *[replace_cost, delete_cost, insert_cost]
                 .iter()
                 .min()
-                .unwrap();
+                .expect("This array obviously isn't empty");
 
-            // If characters are equal
-            if s1.chars().nth(y) != s2.chars().nth(x) {
-                edit_distance += 1;
-            }
+            // If characters are not equal we must perform an operation, which will add 1 to our
+            // cost.
+            // We subtract 1 from the character index because the grid has the empty string as a
+            // possibility in the first row and column.
+            let edit_distance = if s1.chars().nth(y - 1) != s2.chars().nth(x - 1) {
+                min_cost + 1
+            } else {
+                min_cost
+            };
 
             m.set(x, y, edit_distance);
         }
     }
 
-    println!("{}", m);
-
     // The answer is stored in the bottom right cell
     // We know this won't panic, because the width and height of
     // the matrix obviously can't be out of its own bounds.
     *m.get(m.width() - 1, m.height() - 1).unwrap()
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_identical_strings() {
+        assert_eq!(edit_distance("kitten", "kitten"), 0);
+    }
+
+    #[test]
+    fn test_single_character_difference() {
+        assert_eq!(edit_distance("kitten", "sitten"), 1);
+    }
+
+    #[test]
+    fn test_single_character_insertion() {
+        assert_eq!(edit_distance("kitten", "kitteny"), 1);
+    }
+
+    #[test]
+    fn test_single_character_deletion() {
+        assert_eq!(edit_distance("kitten", "kittn"), 1);
+    }
+
+    #[test]
+    fn test_multiple_character_difference() {
+        assert_eq!(edit_distance("kitten", "sitting"), 3);
+    }
+
+    #[test]
+    fn test_empty_to_non_empty_string() {
+        assert_eq!(edit_distance("", "kitten"), 6);
+    }
+
+    #[test]
+    fn test_non_empty_to_empty_string() {
+        assert_eq!(edit_distance("kitten", ""), 6);
+    }
+
+    #[test]
+    fn test_completely_different_strings() {
+        assert_eq!(edit_distance("flaw", "lawn"), 2);
+    }
+
+    #[test]
+    fn test_same_characters_different_order() {
+        assert_eq!(edit_distance("abc", "cba"), 2);
+    }
+
+    #[test]
+    fn test_strings_with_repeated_characters() {
+        assert_eq!(edit_distance("aaaa", "aaa"), 1);
+    }
+
+    #[test]
+    fn test_case_sensitivity() {
+        assert_eq!(edit_distance("Kitten", "kitten"), 1);
+    }
+
+    #[test]
+    fn test_real_world_example() {
+        assert_eq!(edit_distance("intention", "execution"), 5);
+    }
+
+    #[test]
+    fn test_both_strings_empty() {
+        assert_eq!(edit_distance("", ""), 0);
+    }
+
+    #[test]
+    fn test_one_character_to_empty_string() {
+        assert_eq!(edit_distance("a", ""), 1);
+    }
+
+    #[test]
+    fn test_empty_string_to_one_character() {
+        assert_eq!(edit_distance("", "a"), 1);
+    }
 }
