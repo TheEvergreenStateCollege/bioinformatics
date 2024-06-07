a92e14b82203dbf46501d7cfbb9158317b27b048
Gavin Bowers <supercat648@gmail.com>
2024-06-07 11:38:21 -0700

    got read lignment working

--- src/main.rs
+++ src/main.rs
@@ -2,8 +2,8 @@
 use bincode::de::read;
 use clap::{command, Parser};
 use smarty_plants::{
-    // algorithms::better_align::align_fragments,
-    data_structures::{transcriptome::Transcriptome, suffix_tree::SuffixTree},
+    algorithms::read_align::{align_fragment, Match},
+    data_structures::{suffix_tree::{self, SuffixTree}, transcriptome::Transcriptome},
     file_io::fasta::*,
 };
 use std::{fs::{self, read_to_string}, path::PathBuf};
@@ -21,12 +21,24 @@ fn main() {
     // let cli = Cli::parse();
     // human_panic::setup_panic!();
 
-    // let read_dir = std::path::Path::new("data/reads/");
-    // let files = read_directory_to_string(read_dir).expect("failed to read fragment files");
-    // let fragments = parse_file(&files).expect("failed to parse fragments");
-    // let genome = parse_genome(read_to_string("data/ref_genome.fna").expect("failed to read genome file"));
-    // let transcriptome = Transcriptome::new(&genome);
-    //let trans_array = FragmentArray::new(transcriptome.get_bases(), 50);
-    //align_fragments(&fragments, &trans_array, &transcriptome);
+    let read_dir = std::path::Path::new("data/reads/");
+    let files = read_directory_to_string(read_dir).expect("failed to read fragment files");
+    let fragments = parse_file(&files).expect("failed to parse fragments");
+    let genome = parse_genome(read_to_string("data/ref_genome.fna").expect("failed to read genome file"));
+    let transcriptome = Transcriptome::new(&genome);
+    let mut st = suffix_tree::SuffixTree::new();
+    for c in transcriptome.get_bases().chars().map(|x| x as u8).take(80_000_000) {
+        st.extend(c);
+    }
 
+    for i in 0..fragments.len() {
+        let matches = align_fragment(&fragments[i], &st, &transcriptome);
+        let mut best: &Match = &matches[0];
+        for m in matches.iter() {
+            if m.errors < best.errors {
+                best = &m;
+            }
+        }
+        println!("{:?}, read_length: {}", best, &fragments[i].bases().len());
+    }
 }
