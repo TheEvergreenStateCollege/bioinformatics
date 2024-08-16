9051e4d742a448dc80c0204ddec7fc036a984240
AbyssalRemark <dev.dominicsevere@gmail.com>
2024-08-16 13:53:35 -0700

    changed fn pointers to closures, does not work

--- rusty-plants/src/data_structures/loader.rs
+++ rusty-plants/src/data_structures/loader.rs
@@ -1,21 +1,60 @@
 //loader is a container that is used in place of a vector to allow reuse of alocated memory. 
 
-#[derive(Debug)]
-struct Loader<Data, ID>{
-    raw: Vec<Data>,       //data container
+//#[derive(Debug)]
+struct Loader<ID, DATA, LOAD, UNLOAD>
+where 
+    LOAD: FnMut(ID) -> DATA,
+    UNLOAD: FnMut(DATA),
+{
+    raw: Vec<DATA>,       //data container
     index: Vec<ID>,       //stores index values. hash map?
-    load: fn(ID) -> Data, //function for loading data in whatever way
-    unload: fn(Data)      //function for unloading data, depending.
+    load:  LOAD,          //function (closuer) for loading data in whatever way
+    unload: UNLOAD        //function (closuer) for unloading data, depending.
 }
 
-impl<Data, ID> Loader<Data, ID> {
-    pub fn new(load: fn(ID) -> Data, unload: fn(Data)) -> Loader<Data, ID> {
-        let mut l = Loader{
+impl<ID, DATA, LOAD, UNLOAD> Loader<ID, DATA, LOAD, UNLOAD> 
+where 
+    LOAD: FnMut(ID) -> DATA,
+    UNLOAD: FnMut(DATA),
+{
+    pub fn new(load: LOAD, unload: UNLOAD) -> Loader<ID, DATA, LOAD, UNLOAD> {
+        Self {
             raw: Vec::new(),
             index: Vec::new(),
-            load: load,
-            unload: unload
-        };
-        l
+            load,
+            unload
+        }
     }
+
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use std::collections::HashMap;
+
+    #[test]
+    fn init() {
+        let mut storeage: HashMap<u32, i32> = HashMap::new();
+        let mut idcounter = 0;
+
+        let load_test_closer = |ID: u32| -> i32 
+        {
+            match storeage.get(&ID)
+            {
+                Some(i) => *i, 
+                None => panic!("ID not found!"),
+            }
+        };
+
+        let unload_test_closer = |Data: i32 | 
+        {
+            idcounter = idcounter +1;
+            storeage.insert(idcounter, Data);
+        };
+
+        let loader = Loader::new(load_test_closer, unload_test_closer);
+
+    }
+
 }
