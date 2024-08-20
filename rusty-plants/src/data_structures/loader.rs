0d31cb02915bbb843ba6cc2ca26df76e9581c38f
AbyssalRemark <dev.dominicsevere@gmail.com>
2024-08-20 16:16:03 -0700

    renamed loader to store and loadContainer to loader

--- rusty-plants/src/data_structures/loader.rs
+++ rusty-plants/src/data_structures/loader.rs
@@ -1,22 +1,35 @@
 //loadContainer is a container that is used in place of a vector to allow reuse of alocated memory. using a loader
 use num::Integer;
+use std::fmt;
 
-pub trait Loader<ID, DATA> {
+pub trait Store<ID, DATA> {
     fn load(&mut self, _:ID) -> DATA;
     fn unload(&mut self, _:DATA);
 }
 
 //#[derive(Debug)]
-struct LoadContainer<ID: Integer + Default, DATA>
+struct Loader<ID: Integer + Default, DATA>
 {
     raw: Vec<DATA>,                    //data container
     index: Vec<ID>,                    //stores index values. hash map?
-    loader: Box<dyn Loader<ID, DATA>>, //The encapsulation of load and unload.
+    loader: Box<dyn Store<ID, DATA>>, //The encapsulation of load and unload.
     idcount: ID                        //running count of id values
 }
 
-impl<ID: Integer + Default, DATA> LoadContainer<ID, DATA> {
-    pub fn new(loader: impl Loader<ID, DATA> + 'static) -> LoadContainer<ID, DATA> 
+/* Functions used in suffixtrees for loadContainer to replace vec 
+ *
+ *   explicitly used:
+ * 
+ *     * .push       adds thing. increments id
+ *     * []          checks if thing, returns if found. grabs from store if not
+ *     * .len()      returns idcount (I think)
+ *     * .iter_mut()
+ *     * .itter()
+ *
+ */
+
+impl<ID: Integer + Default, DATA> Loader<ID, DATA> {
+    pub fn new(loader: impl Store<ID, DATA> + 'static) -> Loader<ID, DATA> 
     {
         Self {
             raw: Vec::<DATA>::new(),    
@@ -28,39 +41,52 @@ impl<ID: Integer + Default, DATA> LoadContainer<ID, DATA> {
 
 }
 
+//manual impl of trait Debug sense the debug macro can't create something for Box, so we print the pointer.
+impl<ID: Integer + Default + fmt::Debug, DATA: fmt::Debug> fmt::Debug for Loader<ID, DATA> {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        f.debug_struct("Loader")
+            .field("raw", &self.raw)
+            .field("index", &self.index)
+            .field("idcount", &self.idcount)
+            // Print the pointer address of the Box, probably will remove later.
+            .field("loader", &format_args!("{:p}", &*self.loader)) 
+            .finish()
+    }
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
     use std::collections::HashMap;
 
-    struct TestLoader {
+    #[derive(Debug)]
+    struct TestStore {
         map: HashMap<u32,i32>,
-        idcount: u32
     }
 
-    impl TestLoader {
-        pub fn new() -> TestLoader {
+    //test loader trait implimentor
+    impl TestStore {
+        pub fn new() -> TestStore {
             Self {
-                map: HashMap::new(),
-                idcount: 0
+                map: HashMap::new()
             }
         }
     }
 
-    impl Loader<u32, i32> for TestLoader{
+    impl Store<u32, i32> for TestStore{
         fn load(&mut self, id: u32) -> i32 {
             unimplemented!()
         }
 
         fn unload(&mut self, Data: i32) {
-
+            unimplemented!()
         }
     }
 
     #[test]
     fn init() {
-
-        //let loader = LoadContainer::new(load_test_closer, unload_test_closer);
+        let loader = Loader::new(TestStore::new());
+        println!("{:?}", loader);
 
     }
 
